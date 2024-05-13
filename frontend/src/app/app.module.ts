d6cea4cad8f654d70db56b93ef82038b3dc332ad
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-05-13 20:45:23 +0200

    Added a spinner while the vehicles are loading

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,10 @@ import {
 } from './view-reservation-details-dialog/view-reservation-details-dialog.component';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 export const MY_FORMATS = {
@@ -77,6 +81,10 @@ export function apiConfigFactory(): Configuration {
     MatSelectModule,
     MatProgressSpinnerModule,
     MatProgressBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule,
   ],
   providers: [
     {
