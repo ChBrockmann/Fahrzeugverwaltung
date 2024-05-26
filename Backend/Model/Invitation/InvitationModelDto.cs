88900ad6b9a131ab48f35bfa2671f4e6f4a75cc4
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-05-26 23:43:35 +0200

    Added InvitationmodelDto

---
+++ Backend/Model/Invitation/InvitationModelDto.cs
@@ -0,0 +1,20 @@
+﻿using Model.User;
+
+namespace Model.Invitation;
+
+public record InvitationModelDto
+{
+    public InivitationId Id { get; set; } = InivitationId.Empty;
+
+    public string Token { get; set; } = string.Empty;
+
+    public UserDto CreatedBy { get; set; } = new();
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public UserDto? AcceptedBy { get; set; }
+    public DateTime? AcceptedAt { get; set; }
+
+    public List<string> Roles { get; set; } = new();
+
+    public DateTime ExpiresAt { get; set; }
+}
