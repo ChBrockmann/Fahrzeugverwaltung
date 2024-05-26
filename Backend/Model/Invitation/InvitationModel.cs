0c22c06326ddadf2e1c6211d12fe09680bc88fca
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-05-26 23:41:01 +0200

    Added InvitationModel

---
+++ Backend/Model/Invitation/InvitationModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Model.User;
+using StronglyTypedIds;
+
+namespace Model.Invitation;
+
+[StronglyTypedId(Template.Guid)]
+public partial struct InivitationId { }
+
+public record InvitationModel : IDatabaseId<InivitationId>
+{
+    public string Token { get; set; } = string.Empty;
+
+    public UserModel CreatedBy { get; set; } = new();
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public UserModel? AcceptedBy { get; set; }
+    public DateTime? AcceptedAt { get; set; }
+
+    public List<IdentityRole<Guid>> Roles { get; set; } = new();
+
+    public DateTime ExpiresAt { get; set; }
+    public InivitationId Id { get; set; } = InivitationId.Empty;
+}
