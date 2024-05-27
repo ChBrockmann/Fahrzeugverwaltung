ff70406c6edfe1eb0284f65cbca165c833d9d777
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-05-27 19:14:04 +0200

    Added Mappingtests

---
+++ Backend/MappingTests/InvitationMappingTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Model.Invitation;
+using Model.User;
+
+namespace MappingTests;
+
+public class InvitationMappingTests : TestBase
+{
+    [Fact]
+    public void InvitationModel_To_InvitationModelDto()
+    {
+        InvitationModel input = Fixture.Create<InvitationModel>();
+        InvitationModelDto expected = new()
+        {
+            Id = input.Id,
+            Token = input.Token,
+            ExpiresAt = input.ExpiresAt,
+            CreatedBy = Mapper.Map<UserDto>(input.CreatedBy),
+            CreatedAt = input.CreatedAt,
+            AcceptedBy = input.AcceptedBy is not null ? Mapper.Map<UserDto>(input.AcceptedBy) : null,
+            AcceptedAt = input.AcceptedAt,
+            Roles = input.Roles.Select(x => x.Name ?? string.Empty).ToList()
+        };
+
+        InvitationModelDto actual = Mapper.Map<InvitationModelDto>(input);
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
