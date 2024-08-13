eb8f4ff20435d685ec814f3bd1a7b5105ac32556
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-08-13 23:44:04 +0200

    Added Initial Admin User with admin role

--- Backend/Fahrzeugverwaltung/Startup/DatabaseInitialization.cs
+++ Backend/Fahrzeugverwaltung/Startup/DatabaseInitialization.cs
@@ -2,15 +2,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Model;
+using Model.User;
 using Model.Vehicle;
 
 namespace Fahrzeugverwaltung.Startup;
 
 public static class DatabaseInitialization
 {
-    public static void InitializeDatabase(this WebApplication app)
+    public static async Task InitializeDatabase(this WebApplication app)
     {
         DatabaseContext database = app.Services.CreateScope().ServiceProvider.GetRequiredService<DatabaseContext>();
+        var usermanager = app.Services.CreateScope().ServiceProvider.GetRequiredService<UserManager<UserModel>>();
+        
         database.Database.Migrate();
         database.SaveChanges();
 
@@ -28,6 +31,32 @@ public static class DatabaseInitialization
                 Name = Security.AdminRoleName
             });
             database.SaveChanges();
+        }
+
+        if (!database.Users.Any())
+        {
+            string email = "admin@example.com";
+            var result = await usermanager.CreateAsync(new UserModel()
+            {
+                Firstname = "Admin",
+                Lastname = "",
+                Organization = string.Empty,
+                Email = email,
+                UserName = email
+            }, "Admin123!");
+            var dbUser = await usermanager.FindByEmailAsync(email) ?? throw new ArgumentNullException();
+            
+            Console.WriteLine(dbUser.Email);
+            Console.WriteLine(Security.AdminRoleName);
+            
+            database.UserRoles.Add(new IdentityUserRole<Guid>
+            {
+                UserId = dbUser.Id,
+                RoleId = database.Roles.First(x => x.Name == Security.AdminRoleName).Id
+            });
+            await database.SaveChangesAsync();
+            
+            await usermanager.AddToRolesAsync(dbUser, [Security.AdminRoleName]);
         }
     }
 }
