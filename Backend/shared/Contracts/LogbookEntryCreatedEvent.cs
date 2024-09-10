8a562f2e89c8b9773d9d9bec28c111d8553e36d2
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-10 14:49:40 +0200

    Added contracts project

---
+++ Backend/shared/Contracts/LogbookEntryCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Contracts;
+
+public record LogbookEntryCreatedEvent
+{
+    public string ImageAsBase64 { get; set; } = string.Empty;
+    public string ImageType { get; set; } = string.Empty;
+}
