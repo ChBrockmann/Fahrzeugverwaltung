2ce9ed7fcecea5edb8d9102c5350d2c03da884e0
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-10 15:33:06 +0200

    Added basic stuff

---
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Service/Program.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using ValidateRouteMileage.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Configuration
+    .AddJsonFile("./configuration/appsettings.json", true, true)
+    .AddUserSecrets(typeof(Program).Assembly, true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.Configure<Configuration>(builder.Configuration);
+Configuration? configuration = builder.Configuration.Get<Configuration>();
+if (configuration is null) throw new Exception("Configuration is null");
+
+var rabbitMqConfiguration = configuration.RabbitMq;
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumers(typeof(Program).Assembly);
+
+    config.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(rabbitMqConfiguration.Host, rabbitMqConfiguration.Port, rabbitMqConfiguration.VirtualHost, c =>
+        {
+            c.Username(rabbitMqConfiguration.Username);
+            c.Password(rabbitMqConfiguration.Password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+app.Run();
