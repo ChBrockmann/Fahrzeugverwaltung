95777ca3d57c5d3227373ebd2e7cfbb3bcc51a6e
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-12 16:37:35 +0200

    Added RouteValidationParamaters to config

--- microservices/ValidateRouteMileage/ValidateRouteMileage.Model/Configuration/Configuration.cs
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Model/Configuration/Configuration.cs
@@ -1,8 +1,10 @@
 ﻿namespace ValidateRouteMileage.Model.Configuration;
 
-public record Configuration
+public sealed record Configuration
 {
     public RabbitMqConfiguration RabbitMq { get; set; } = new();
 
     public GoogleMapsConfiguration GoogleMaps { get; set; } = new();
+    
+    public RouteValidationParametersConfiguration RouteValidationParameters { get; set; } = new();
 }
---
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Model/Configuration/RouteValidationParametersConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ValidateRouteMileage.Model.Configuration;
+
+public sealed record RouteValidationParametersConfiguration
+{
+    public int AdditionalDistancePerDayInKm { get; set; }
+    public int GrantAdditionalDistanceAfterDays { get; set; }
+    public int AllowedDeviationPercent { get; set; }
+}
