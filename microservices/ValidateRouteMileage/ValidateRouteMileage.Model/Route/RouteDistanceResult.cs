9c60b582da86835b809c6d449f2f6b1af6d8e85c
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-12 16:06:56 +0200

    Added Googlemaps api

---
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Model/Route/RouteDistanceResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidateRouteMileage.Model.Route;
+
+public record RouteDistanceResult
+{
+    public int TotalDistanceInMeter { get; set; }
+
+    public string OriginAdress { get; set; } = string.Empty;
+
+    public string DestinationAdress { get; set; } = string.Empty;
+}
---
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Service/ExternalServices/IGoogleMapsRouteApi.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+
+namespace ValidateRouteMileage.Service.ExternalServices;
+
+internal interface IGoogleMapsRouteApi
+{
+    [Get("maps/api/distancematrix/json")]
+    Task<RouteDistanceResult> GetRouteDistance(string destinations, string origins, string units, string key);
+}
