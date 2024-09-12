7cdc9f758ecc61178bf60444408326234714ce0e
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-12 20:48:43 +0200

    Implement consumer

--- Backend/shared/Contracts/LogbookImageAnalyzed.cs
+++ Backend/shared/Contracts/LogbookImageAnalyzed.cs
@@ -2,5 +2,10 @@
 
 public class LogbookImageAnalyzed
 {
-    public int DetectedTotalMileage { get; set; }
+    public int TotalMileageForRoute { get; set; }
+    public DateOnly StartDateInclusive { get; set; }
+    public DateOnly EndDateInclusive { get; set; }
+
+    public string OriginAdress { get; set; } = string.Empty;
+    public string DestinationAdress { get; set; } = string.Empty;
 }
--- microservices/ValidateRouteMileage/ValidateRouteMileage.Service/Consumer/LogbookImageAnalyzedConsumer.cs
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Service/Consumer/LogbookImageAnalyzedConsumer.cs
@@ -1,12 +1,30 @@
 ﻿using Contracts;
 using MassTransit;
+using ValidateRouteMileage.BusinessLogic;
+using ValidateRouteMileage.Service.ExternalServices.GoogleMaps;
 
 namespace ValidateRouteMileage.Service.Consumer;
 
 public class LogbookImageAnalyzedConsumer : IConsumer<LogbookImageAnalyzed>
 {
+    private readonly IGoogleMapsServiceWrapper _googleMapsService;
+    private readonly AllowedMaximumMileageCalculator _calculator;
+    
+
+    public LogbookImageAnalyzedConsumer(IGoogleMapsServiceWrapper googleMapsService, AllowedMaximumMileageCalculator calculator)
+    {
+        _googleMapsService = googleMapsService;
+        _calculator = calculator;
+    }
+
     public async Task Consume(ConsumeContext<LogbookImageAnalyzed> context)
     {
-        Console.WriteLine("Received: {0}", context.Message.DetectedTotalMileage);
+        var googleMapsResult = await _googleMapsService.GetRouteDistance(context.Message.OriginAdress, context.Message.DestinationAdress);
+
+        var maximumAllowedMileage = _calculator.Calculate(context.Message.StartDateInclusive, context.Message.EndDateInclusive, googleMapsResult);
+
+        string result = context.Message.TotalMileageForRoute > maximumAllowedMileage ? "Exceeded" : "Within limits";
+        
+        Console.WriteLine($"Result: {result}");
     }
 }
