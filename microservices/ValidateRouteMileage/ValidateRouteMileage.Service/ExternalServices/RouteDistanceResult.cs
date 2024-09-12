ec3cf886c9489d345479099ef9c611806dbf1e67
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-12 16:06:56 +0200

    Added Googlemaps api

---
+++ microservices/ValidateRouteMileage/ValidateRouteMileage.Service/ExternalServices/RouteDistanceResult.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ValidateRouteMileage.Service.ExternalServices;
+
+public record RouteDistanceResult
+{
+    [JsonPropertyName("destination_addresses")]
+    public List<string> DestinationAddresses { get; set; } = new();
+
+    [JsonPropertyName("origin_addresses")]
+    public List<string> OriginAddresses { get; set; } = new();
+
+    [JsonPropertyName("rows")]
+    public List<Row> Rows { get; set; } = new();
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
+}
+
+public class Row
+{
+    [JsonPropertyName("elements")]
+    public List<Element> Elements { get; set; } = new();
+}
+
+public class Element
+{
+    [JsonPropertyName("distance")]
+    public Distance Distance { get; set; } = new();
+
+    [JsonPropertyName("duration")]
+    public Duration Duration { get; set; } = new();
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
+}
+
+public class Distance
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; } = string.Empty;
+
+    [JsonPropertyName("value")]
+    public int Value { get; set; }
+}
+
+public class Duration
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; } = string.Empty;
+
+    [JsonPropertyName("value")]
+    public int Value { get; set; }
+}
