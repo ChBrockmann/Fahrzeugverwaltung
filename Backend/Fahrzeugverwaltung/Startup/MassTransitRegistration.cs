5c7c3529bf4d90786be8dc3f0ddb15c654ffe307
ChBrockmann <55225597+ChBrockmann@users.noreply.github.com>
2024-09-12 21:32:27 +0200

    Added Masstransit

---
+++ Backend/Fahrzeugverwaltung/Startup/MassTransitRegistration.cs
@@ -0,0 +1,11 @@
+﻿using MassTransit;
+
+namespace Fahrzeugverwaltung.Startup;
+
+public static class MassTransitRegistration
+{
+    public static void RegisterMassTransit(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddMassTransit(x => { x.UsingInMemory((context, cfg) => { cfg.ConfigureEndpoints(context); }); });
+    }
+}
